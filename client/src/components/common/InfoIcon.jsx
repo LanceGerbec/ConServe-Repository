0650584a57b53036cbe5204700e485c589603a01
Lance Gerbec Abesamis <your-email@gmail.com>
2025-12-27 11:08:00 +0800

    submit

--- client/src/components/common/InfoIcon.jsx
+++ client/src/components/common/InfoIcon.jsx
@@ -3,7 +3,7 @@ import Tooltip from './Tooltip';
 
 const InfoIcon = ({ content, size = 16 }) => (
   <Tooltip content={content}>
-    <button className="inline-flex p-1 hover:bg-blue-100 dark:hover:bg-blue-900/30 rounded-full transition">
+    <button type="button" className="inline-flex p-1 hover:bg-blue-100 dark:hover:bg-blue-900/30 rounded-full transition">
       <Info size={size} className="text-blue-600 dark:text-blue-400" />
     </button>
   </Tooltip>
